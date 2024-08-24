1f05221e537cf3e2c1b6474ad122ad44765ebed6
august-dev-pro <augustinselete01@gmail.com>
2024-08-24 10:18:21 +0000

    first version of the API 001

--- src/express.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request } from "express";
-import { IUser } from "./models/userModel";
-
-declare module "express-serve-static-core" {
-  interface Request {
-    user?: IUser; // Remplace UserDocument par le type correct de ton utilisateur
-  }
-}

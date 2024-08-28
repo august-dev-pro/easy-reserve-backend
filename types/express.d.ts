0529d9996bb16e78f302e7884a7e41f8e15df653
august-dev-pro <augustinselete01@gmail.com>
2024-08-28 14:41:20 +0000

    first version of the API 01

---
+++ types/express.d.ts
@@ -0,0 +1,9 @@
+// types/express.d.ts
+import { Request } from "express";
+import { IUser } from "../src/models/userModel";
+
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: IUser; // Remplacez `any` par le type correct si vous avez un type spécifique pour `user`
+  }
+}

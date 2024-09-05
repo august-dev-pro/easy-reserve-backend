cc7a3d816d03cb08e2d7d0e55d15c1622b23a9dc
august-dev-pro <augustinselete01@gmail.com>
2024-09-05 16:52:42 +0000

    first version of the API 002

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -28,8 +28,6 @@ const loginUser = async (req: Request, res: Response, next: NextFunction) => {
 };
 
 const logoutUser = async (req: Request, res: Response, next: NextFunction) => {
-  console.log(req.cookies);
-
   try {
     const token = req.cookies?.authToken; // Assure-toi que le token est extrait correctement depuis le cookie
 

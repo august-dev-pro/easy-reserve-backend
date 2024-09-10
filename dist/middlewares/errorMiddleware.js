28b4ab2316a3b003ec5666428accb36dd3161245
august-dev-pro <augustinselete01@gmail.com>
2024-09-10 17:38:33 +0000

    first version of the API 003

--- dist/middlewares/errorMiddleware.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const errorHandle_1 = require("../utils/errorHandle");
-const errorMiddleware = (err, req, res, next) => {
-    (0, errorHandle_1.handleError)(err, res);
-};
-exports.default = errorMiddleware;

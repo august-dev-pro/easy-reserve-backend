51566675964d3416463925a3873eb2548c2ce9fe
august-dev-pro <augustinselete01@gmail.com>
2024-10-10 12:28:07 +0000

    prevent test version of easyreserve api version 1.01

--- src/app.ts
+++ src/app.ts
@@ -35,8 +35,7 @@ app.use(cors(corsOptions));
 app.use((req, res, next) => {
   res.setHeader(
     "Access-Control-Allow-Origin",
-    "http://localhost:3000"
-
+    "https://esea-reserve.vercel.app"
     /*     "https://esea-reserve.vercel.app" , "https://easy-reserve-backend-production.up.railway.app",
       "https://esea-reserve.vercel.app", */ // Ajoutez l'URL de production de votre frontend
   );
